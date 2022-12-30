12d81a7f4a3c88ff137c36443c29ab04fe0446ab
anithav <anithav0909@gmail.com>
2022-12-30 14:25:52 +0530

    file updated

--- frontend/dist/foodappangular/polyfills.js
+++ frontend/dist/foodappangular/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\HP\OneDrive\Desktop\FoodAppAngular\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\HP\OneDrive\Desktop\FoodAppAngular\frontend\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
